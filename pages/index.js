a1a33d9d6edb3936c2512c00167269bc478d4c4c
Nijanshu <46223306+Nijanshu@users.noreply.github.com>
2023-11-28 17:40:30 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home(props) {
   return (
     <>
       <Head>
-        <title>inBlog</title>
+        <title>BioStem</title>
         <meta name="description" content="inBlog" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/BioStem.png" />
