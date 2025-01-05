6116091a0e7f5e895c9b8c9c5f90a2c0d0cc61db
Nijanshu <46223306+Nijanshu@users.noreply.github.com>
2025-01-05 10:48:58 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,15 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+   
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-CE4HRJ9P97"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-CE4HRJ9P97');
+</script>
       <body>
         <Main />
         <NextScript />
