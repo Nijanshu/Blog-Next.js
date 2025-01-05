28fe137ac8453a6ec6e5995a4a89985fa9e315a0
Nijanshu <46223306+Nijanshu@users.noreply.github.com>
2025-01-05 10:53:05 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,22 +1,28 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-   
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-CE4HRJ9P97"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-CE4HRJ9P97');
-</script>
+      <Head>
+        {/* Google Analytics script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-CE4HRJ9P97"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-CE4HRJ9P97');
+          `}
+        </Script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
